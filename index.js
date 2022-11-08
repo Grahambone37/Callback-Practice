cac75702d7b0d0061867bb36ced6dfca972018d6
Grahambone37 <electricgrunt37@gmail.com>
2022-11-08 13:12:34 -0800

    added move character function and added to move.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+
 move(character).to(100, 250)
 
 
@@ -13,3 +14,6 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+
